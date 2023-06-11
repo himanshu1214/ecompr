3f6be3343b9bbcf5de67c031aef5a75ea2fe7dc2
himanshu1214 <himanshu90gupta@outlook.com>
2023-06-10 21:46:52 -0500

    add env vars and ECMAScripts modules

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
-const express = require('express');
-const products = require('./data/products');
+import express from 'express';
+
+import dotenv from 'dotenv';
+import products from './data/products.js';
 
 const app = express();
+
+dotenv.config();
 
 app.get('/', (req, res) => {
     res.send('API is running ...');
@@ -17,4 +21,6 @@ app.get('/api/products/:id', (req, res) => {
     res.json(product);
 })
 
-app.listen(5000, console.log('App is running on port 5000'));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`App is running in ${process.env.NODE_ENV} on port ${PORT}`));
