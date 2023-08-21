6ba5295b8da87b60b064133ed6d95503b09186d9
himanshu1214 <himanshu90gupta@outlook.com>
2023-08-21 02:17:49 -0500

    add reducer for cart

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,14 +1,16 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 import { BASE_URL } from '../constants.js';
+import cartSliceReducer from './cartSlice.js';
 
 const baseQuery = fetchBaseQuery({baseUrl: BASE_URL}); // hook to fetch data
 
-console.log(`fetch-base-query: ${baseQuery}`)
 
 // Parent slice which will be for all other slice such as Product, Order, User
 export const apiSlice = createApi({
     baseQuery: baseQuery,
     tagTypes: ['Product', 'Order', 'User'], // allows to specify the caching behavior of each endpoints 
+    reducer: { [apiSlice.reducerPath] : apiSlice.reducer,
+                cartSlice: cartSliceReducer},
     endpoints: (builder) => ({ })
 });
